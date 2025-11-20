25814b8194f2471d409c8e2c97efdace33d2a78c
Volodymyr Kohut <vkohut@hydrolix.io>
2025-11-20 13:51:02 +0000

    GRAP-135 use labels in QueryEditor.tsx

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -35,6 +35,7 @@ import {
 } from "../constants";
 
 import { css } from "@emotion/css";
+import allLabels from "../labels";
 
 export type Props = QueryEditorProps<
   DataSource,
@@ -43,6 +44,7 @@ export type Props = QueryEditorProps<
 >;
 
 export function QueryEditor(props: Props) {
+  const labels = allLabels.components.query.editor;
   const alertStyle = css`
     div:has(> div[role="alert"]) {
       min-width: 10rem;
@@ -184,8 +186,8 @@ export function QueryEditor(props: Props) {
                 <InlineField
                   data-testid="data-testid query type"
                   label={
-                    <InlineLabel width={15} tooltip="Set query type">
-                      Query Type
+                    <InlineLabel width={15} tooltip={labels.queryType.tooltip}>
+                      {labels.queryType.label}
                     </InlineLabel>
                   }
                 >
@@ -201,11 +203,8 @@ export function QueryEditor(props: Props) {
                   error={"invalid duration"}
                   invalid={invalidDuration.current}
                   label={
-                    <InlineLabel
-                      width={10}
-                      tooltip="Round $from and $to timestamps to the nearest multiple of the specified value (1m rounds to the nearest whole minute). Supports time units: ms, s, m, h. No value means that the default round value will be used. A value of 0 means no rounding is applied"
-                    >
-                      Round
+                    <InlineLabel width={10} tooltip={labels.round.tooltip}>
+                      {labels.round.label}
                     </InlineLabel>
                   }
                 >
@@ -217,25 +216,33 @@ export function QueryEditor(props: Props) {
                   />
                 </InlineField>
                 {showSql ? (
-                  <Button icon="eye-slash" onClick={() => setShowSql(false)}>
-                    Hide Interpolated Query
+                  <Button
+                    variant={"secondary"}
+                    icon="eye-slash"
+                    onClick={() => setShowSql(false)}
+                  >
+                    {labels.hideInterpolatedQuery.label}
                   </Button>
                 ) : (
-                  <Button icon="eye" onClick={() => setShowSql(true)}>
-                    Show Interpolated Query
+                  <Button
+                    variant={"secondary"}
+                    icon="eye"
+                    onClick={() => setShowSql(true)}
+                  >
+                    {labels.showInterpolatedQuery.label}
                   </Button>
                 )}
                 <div style={{ marginLeft: "auto", order: 2, display: "table" }}>
                   <ToolbarButton
                     style={{ display: "table-cell" }}
-                    tooltip="Format query"
+                    tooltip={labels.formatQuery.tooltip}
                     onClick={formatQuery}
                   >
                     <Icon name="brackets-curly" />
                   </ToolbarButton>
                   <ToolbarButton
                     style={{ display: "table-cell" }}
-                    tooltip="Click or hit CTRL/CMD+Return to run query"
+                    tooltip={labels.runQuery.tooltip}
                     onClick={props.onRunQuery}
                   >
                     <Icon name="play" />
